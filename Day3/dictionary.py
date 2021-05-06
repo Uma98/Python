eb6911d9fa2b4cd08f1c7c038cf01516abdc6c29
Uma98 <35659097+Uma98@users.noreply.github.com>
2021-05-06 10:33:53 +0530

    Add files via upload

---
+++ Day3/dictionary.py
@@ -0,0 +1,4 @@
+python  = {"name": "python", "ext": "py", "creator": "guido"}
+
+for i in python:
+    print(i + ":" + python[i])
---
+++ Day3/namesList.py
@@ -0,0 +1,5 @@
+list = ["john", "jake", "jack", "george", "jenny", "jason"]
+
+for i in list:
+    if len(i) < 5 and "e" in i:
+        print(i)
