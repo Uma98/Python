a9fee85fe120ceff7e2e2d252f9e6f72e03cd6dd
Uma98 <35659097+Uma98@users.noreply.github.com>
2021-05-06 09:28:57 +0530

    Update inputList.py
    Added comments

--- Day2/inputList.py
+++ Day2/inputList.py
@@ -1,13 +1,18 @@
 input_string = input("Enter a list element separated by space ")
 list  = input_string.split()
 print("Calculating sum of element of input list")
+#sum of all numbers
 sum = 0
 for num in list:
     sum += int (num)
 print("Sum = ",sum)
+#minimum 
 print(min(list))
+#eliminating duplictes
 print(set(list))
+#sorting in ascending order
 list.sort()
 print(list)
+#descending sort
 list.sort(reverse = True)
 print(list)
